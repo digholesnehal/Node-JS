a7a16f0abf3c7868dd8db03cfee5aa4921218df0
digholesnehal <digholesnehal@gmail.com>
2020-05-31 13:09:35 +0530

    console message

--- Assignment1/app.js
+++ Assignment1/app.js
@@ -1,6 +1,6 @@
 Name=require("./Notes.js")
 
-console.log("In app.js", Name)
+console.log("In app.js, trying to fetch name", Name)
 
 // const fs = require("fs");
 // fs.writeFileSync("Notes.txt", "I am Snehal.")
