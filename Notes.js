ce6000076c97cc4232bf3e0c5e25a107178e37b7
digholesnehal <digholesnehal@gmail.com>
2020-05-31 13:14:54 +0530

    only assignment 1 folder, removed extra files

--- Notes.js
+++
@@ -1,5 +0,0 @@
-console.log("In Notes.js")
-
-const Name = "Snehal"
-
-module.export = Name
--- app.js
+++
@@ -1,7 +0,0 @@
-Name=require("./Notes.js")
-
-console.log("In app.js", Name)
-
-// const fs = require("fs");
-// fs.writeFileSync("Notes.txt", "I am Snehal.")
-// fs.appendFileSync("Notes.txt", " I am learning Node.js")
